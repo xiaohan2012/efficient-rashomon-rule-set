350074cd35696e77bd8d96e9392fb836931309d9
Han Xiao <xiaohan2012@gmail.com>
2023-03-09 07:13:24 +0200

    a draft implementation of fpgrowth

--- bds/fpgrowth.py
+++ bds/fpgrowth.py
@@ -1,17 +1,25 @@
 from dataclasses import dataclass
 from collections import OrderedDict, defaultdict, Counter
-from typing import List, Set, Optional, Dict
+
+from typing import List, Set, Optional, Dict, Tuple
+
+from .utils import powerset
 
 Item = int
+Freq = int
+ItemFreq = Tuple[Item, Freq]
 Transaction = Set[Item]
-OrderedTransaction = OrderedDict
+TransactionDict = Dict[Item, Freq]
+TransactionDictList = List[TransactionDict]
+OrderedTransaction = List[Tuple[Item, Freq]]
 Database = List[Transaction]
 
+TransactionList = List[Transaction]
 ROOT = -1
 
 
+HeaderTable = Dict[Item, List["FPTree"]]
 
-HeaderTable = Dict[Item, List['FPTree']]
 
 @dataclass
 class FPTree:
@@ -30,13 +38,20 @@ class FPTree:
             return False
 
     @property
-    def all_names(self):
+    def is_root(self):
+        return self.parent is None
+    
+    def all_names(self, exclude_root=False):
         if self.is_leaf:
             return {self.name}
         else:
-            ret = {self.name}
+            ret = (
+                set()
+                if self.is_root and exclude_root
+                else {self.name}
+            )
             for c in self.children:
-                ret |= c.all_names
+                ret |= c.all_names(exclude_root=exclude_root)
             return ret
 
     @property
@@ -51,11 +66,19 @@ class FPTree:
             return tuple()
 
     @property
-    def item_count_on_path_to_root(self):
-        if self.parent is not None:
-            return self.parent.item_count_on_path_to_root + ((self.name, self.count),)
+    def item_count_on_path_to_root(self) -> List[Tuple[Item, int]]:
+        """
+        traversing the path from the current node to the root (excluding the root)
+        and collect (item, count) information
+
+        note that root is excluded
+        """
+        if self.is_root:
+            return []
+        elif self.parent is not None:
+            return self.parent.item_count_on_path_to_root + [(self.name, self.count)]
         else:
-            return ((self.name, self.count), )
+            return [(self.name, self.count)]
 
     @property
     def children(self):
@@ -81,31 +104,50 @@ class FPTree:
     def __repr__(self):
         return "{}:{}".format(self.name, self.count)
 
+    def as_nested_tuple(self):
+        """
+        return the tree as a nested tuple
 
-def insert_tree(head, remain, tree):
-    """insert an itemset (head + remain) into the tree"""
+        ((name, count), (child0, child1, ...))
+        """
+        ret = ((self.name, self.count), list())
+        for child in self.children:
+            ret[1].append(child.as_nested_tuple())
+        return ret
+
+
+def insert_tree(head: Tuple[Item, int], remain, tree):
+    """insert an itemset (head + remain) into the tree
+
+    head is the first item in the itemset, together with its frequency
+    """
     # if the node has a child with the same name
     head_name, head_count = head
     for child in tree.children:
         if child.name == head_name:
             child.count += head_count
             break
-
     else:
         # otherwise, create a new child
         child = FPTree(name=head_name, count=head_count, parent=tree)
         tree.add_child(child)
 
     if len(remain) > 0:
+        # recurse on
         insert_tree(remain[0], remain[1:], child)
 
 
 # build the FPtree
-def build_fptree(preprocessed_trans_list):
+def build_fptree(preprocessed_trans_list: List[OrderedTransaction]) -> FPTree:
+    """the input transaction list should already filter out infrequent items
+    and items in each transaction should be ordered by their frequency in descending order
+    """
     root = FPTree(name="ROOT")
 
     for trans in preprocessed_trans_list:
-        insert_tree(trans[0], trans[1:], root)
+        print("trans: ", trans)
+        if len(trans) > 0:
+            insert_tree(trans[0], trans[1:], root)
 
     return root
 
@@ -125,7 +167,7 @@ def build_header_table(tree):
     return header_table
 
 
-def add_frequency_to_transaction_list(trans_list):
+def add_frequency_to_transaction_list(trans_list: TransactionList) -> TransactionDictList:
     """
     add frequency information to each item in each transaction
 
@@ -138,7 +180,7 @@ def add_frequency_to_transaction_list(trans_list):
     return trans_list_with_freq
 
 
-def extract_frequent_items(trans_list_with_freq, min_support) -> List:
+def extract_frequent_items(trans_list_with_freq: List[TransactionDict], min_support: int) -> List[Item]:
     """extract items whose frequency to at least min_support
 
     return the frequent items ordered by their frequency in descending order
@@ -158,7 +200,9 @@ def extract_frequent_items(trans_list_with_freq, min_support) -> List:
     return ordered_frequent_items
 
 
-def filter_and_reorder_transaction_list(trans_list_with_freq, ordered_frequent_items: List):
+def filter_and_reorder_transaction_list(
+    trans_list_with_freq: List[TransactionDict], ordered_frequent_items: List[Item]
+) -> List[OrderedTransaction]:
     """
     convert the transactions to ordered and filtered transactions
     """
@@ -173,10 +217,25 @@ def filter_and_reorder_transaction_list(trans_list_with_freq, ordered_frequent_i
         preprocessed_trans_list.append(preprocessed_trans)
     return preprocessed_trans_list
 
-def squash_path_by_leaf_frequency(path):
+
+def preprocess_transaction_list(trans_list: List[Transaction], min_support: int) -> List[OrderedTransaction]:
+    """given a list of "raw" transactions and minimum support,
+
+    convert each transaction into an ordered form, where only the frequent items are included
+    and they are furthered ordered by their frequency
+    """
+    transactions_with_freq = add_frequency_to_transaction_list(trans_list)
+
+    frequent_items = extract_frequent_items(transactions_with_freq, min_support)
+
+    ordered_input_data = filter_and_reorder_transaction_list(transactions_with_freq, frequent_items)
+
+    return ordered_input_data
+
+def squash_path_by_leaf_frequency(path: List[ItemFreq]) -> List[ItemFreq]:
     """
     given a list of item, count pairs, change the count of each entry  to that of the last entry in the list
-    
+
     (('f', 4), ('c', 3), ('a', 3), ('m', 2), ('p', 2))
 
     ->
@@ -185,3 +244,40 @@ def squash_path_by_leaf_frequency(path):
     """
     tail_freq = path[-1][1]
     return tuple((item[0], tail_freq) for item in path)
+
+
+def fpgrowth_on_tree(tree: FPTree, prefix: set, min_support: int):
+    if tree.is_path:
+        # enumerate all combinations
+        all_names = set(prefix) | tree.all_names(exclude_root=True)
+        # print("prefix: ", prefix)
+        # print("tree: \n", tree)
+        # return list(
+        yield from map(lambda seq: tuple(sorted(seq)), powerset(all_names, min_size=1))
+        # )
+        # for frequent_itemset in powerset(all_names, min_size=1):
+        #     yield frequent_itemset
+    else:
+        header_table = build_header_table(tree)
+        # print("header_table: ", header_table)
+        # construct conditional tree
+        for item in header_table.keys():
+            paths = [node.item_count_on_path_to_root for node in header_table[item]]
+            cond_trans_list = list(map(squash_path_by_leaf_frequency, paths))
+
+            cond_trans_list = list(map(dict, cond_trans_list))  # turn to dict
+
+            cond_frequent_items = extract_frequent_items(
+                cond_trans_list, min_support=min_support
+            )
+
+            cond_preprocessed_trans_list = filter_and_reorder_transaction_list(
+                cond_trans_list, cond_frequent_items
+            )
+
+            # print("prefix: ", prefix)
+            # print("cond_preprocessed_trans_list: ", cond_preprocessed_trans_list)            
+            cond_tree = build_fptree(cond_preprocessed_trans_list)
+            
+            # cond_header_table = build_header_table(tree)
+            yield from fpgrowth_on_tree(cond_tree, prefix | {item}, min_support)
--- tests/test_fpgrowth.py
+++ tests/test_fpgrowth.py
@@ -1,10 +1,16 @@
 import pytest
+import itertools
+from bds.utils import powerset
 from bds.fpgrowth import (
     FPTree,
     add_frequency_to_transaction_list,
     extract_frequent_items,
     filter_and_reorder_transaction_list,
     squash_path_by_leaf_frequency,
+    insert_tree,
+    build_fptree,
+    fpgrowth_on_tree,
+    preprocess_transaction_list,
 )
 
 
@@ -17,6 +23,18 @@ def a_path():
 
 
 class TestFPTree:
+    def test_as_nested_tuple(self, a_path):
+        actual = a_path.as_nested_tuple()
+        expected = ((0, 3), [((1, 2), [((2, 1), [])])])  # root  # children
+        assert actual == expected
+
+        a_path.add_child(FPTree(3, 1))
+        expected = (
+            (0, 3),  # root
+            [((1, 2), [((2, 1), [])]), ((3, 1), [])],  # children
+        )
+        actual = a_path.as_nested_tuple()
+
     def test_add_child(self):
         root = FPTree(0, count=3)
         child = root.add_child(FPTree(1, count=2))
@@ -48,17 +66,21 @@ class TestFPTree:
 
     def test_all_names(self):
         root = FPTree(0)
-        assert root.all_names == {0}
+        assert root.all_names(exclude_root=False) == {0}
         child = root.add_child(FPTree(1))
-        assert root.all_names == {0, 1}
+        assert root.all_names(exclude_root=False) == {0, 1}
         child.add_child(FPTree(2))
-        assert root.all_names == {0, 1, 2}
+        assert root.all_names(exclude_root=False) == {0, 1, 2}
         root.add_child(FPTree(3))
-        assert root.all_names == {0, 1, 2, 3}
+        assert root.all_names(exclude_root=False) == {0, 1, 2, 3}
+
+        # now we exclude the root
+        assert root.all_names(exclude_root=True) == {1, 2, 3}
 
     def test_item_count_on_path_to_root(self, a_path):
         a_leaf = a_path.children[0].children[0]
-        assert a_leaf.item_count_on_path_to_root == ((0, 3), (1, 2), (2, 1))
+        # (0, 3),  should be excluded
+        assert a_leaf.item_count_on_path_to_root == [(1, 2), (2, 1)]
 
 
 class Test_add_frequency_to_transaction_list:
@@ -113,8 +135,87 @@ class Test_squash_path_by_leaf_frequency:
 
 
 class Test_insert_tree:
-    pass
+    def test_no_new_node(self, a_path):
+        # a_path: (0, 2), (1, 2), (2, 1)
+        itemset = [(1, 1), (2, 1)]
+        head, tail = itemset[0], itemset[1:]
+
+        # no new node is created, we simply increment the count
+        insert_tree(head, tail, a_path)
+
+        actual = a_path.as_nested_tuple()
+        expected = ((0, 3), [((1, 3), [((2, 2), [])])])
+        assert actual == expected
+
+    def test_with_new_node(self, a_path):
+        itemset = [(2, 1), (1, 1)]
+        head, tail = itemset[0], itemset[1:]
+
+        # two new nodes are created
+        insert_tree(head, tail, a_path)
+
+        actual = a_path.as_nested_tuple()
+        expected = (
+            (0, 3),
+            [
+                ((1, 2), [((2, 1), [])]),
+                ((2, 1), [((1, 1), [])]),  # the new branch (and nodes)
+            ],
+        )
+        assert actual == expected
+
+
+class Test_build_fptree:
+    def test_simple(self):
+        input_data = [[(0, 1), (1, 1), (2, 1)], [(0, 1), (2, 1), (1, 1)]]
+        tree = build_fptree(input_data)
+        actual = tree.as_nested_tuple()
+        expected = (
+            ("ROOT", 0),
+            [((0, 2), [((1, 1), [((2, 1), [])]), ((2, 1), [((1, 1), [])])])],
+        )
+        assert actual == expected
 
 
 class TestFPGrowth:
-    pass
+    def test_single_node(self):
+        single_node_tree = FPTree("ROOT", 1)
+        single_node_tree.add_child(FPTree(0, 1))
+
+        actual = set(fpgrowth_on_tree(single_node_tree, set(), 1))
+        expected = set([(0,)])
+        assert actual == expected
+
+    def test_a_path(self):
+        path = FPTree("ROOT", 1)
+        child = path.add_child(FPTree(0, 1))
+        child.add_child(FPTree(1, 1))
+
+        actual = set(fpgrowth_on_tree(path, set(), 1))
+        expected = set([(0,), (1,), (0, 1)])
+        assert actual == expected
+
+    @pytest.mark.parametrize('min_support, expected',
+                             [
+                                 (2, powerset({0, 1}, min_size=1)),
+                                 (1, itertools.chain(
+                                     powerset({0, 1, 2}, min_size=1),
+                                     powerset({0, 1, 3}, min_size=1),
+                                 ))
+                             ])
+    def test_allgether(self, min_support, expected):
+        input_transactions = [{0, 1, 2}, {0, 1, 3}]
+
+        ordered_input_data = preprocess_transaction_list(
+            input_transactions, min_support
+        )
+        
+        tree = build_fptree(ordered_input_data)
+        actual = set(fpgrowth_on_tree(tree, set(), min_support))
+        expected = set(
+            map(
+                lambda seq: tuple(sorted(seq)),
+                expected
+            )
+        )
+        assert actual == expected
