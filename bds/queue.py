073d16dcfd5268890fe3f5bc98a3236a78c1dfdd
Han Xiao <xiaohan2012@gmail.com>
2023-04-23 10:28:39 +0800

    queue implementation

--- bds/queue.py
+++ bds/queue.py
@@ -1,19 +1,31 @@
-import numpy as np
-
-from .cache_tree import Node
+import heapq
+from typing import Any, Union
 
 
 class Queue:
-    """a priority queue for branch-and-bound search"""
-    def __init__(self):
-        pass
-        
-    def push(self, node: Node, not_captured: np.ndarray):
-        pass
+    """a priority queue (as a max heap) for branch-and-bound search"""
 
-    def front(self):
-        pass
-    
-    def pop(self):
-        pass
+    def __init__(self):
+        """a simple queue implementation"""
+        self._items = []
+
+    def push(self, item: Any, key: Union[float, int]):
+        item_with_key = (-key, item)  # negate the key so that it is a max heap
+        heapq.heappush(self._items, item_with_key)
+
+    def front(self) -> Any:
+        key, item = self._items[0]
+        return item
+
+    def pop(self) -> Any:
+        key, item = heapq.heappop(self._items)
+        return item
+
+    @property
+    def size(self) -> int:
+        return len(self._items)
+
+    @property
+    def is_empty(self) -> bool:
+        return self.size == 0
         
--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,13 +1,38 @@
+import numpy as np
 import pytest
 from bds.queue import Queue
+from bds.utils import randints
 
 
 class TestQueue:
-    def test_push(self):
-        pass
+    def get_simple_queue(self, seed=None):
+        np.random.seed(seed)
+        self.items_with_key = [("first", 100), ("second", 10), ("third", 1)]
+        self.items_with_key_permutated = self.items_with_key.copy()
+        np.random.shuffle(self.items_with_key_permutated)
 
-    def test_pop(self):
-        pass
+        q = Queue()
 
-    def test_front(self):
-        pass
+        print("self.items_with_key_permutated: {}".format(self.items_with_key_permutated))
+        for item, key in self.items_with_key_permutated:
+            q.push(item, key=key)
+
+        return q
+
+    @pytest.mark.parametrize("seed", randints(1))
+    def test_all(self, seed):
+        q = self.get_simple_queue(seed)
+
+        total_queue_size = len(self.items_with_key)
+        assert q.size == total_queue_size
+        assert not q.is_empty
+
+        for i, expected_item_and_key in zip(
+            range(q.size), self.items_with_key
+        ):
+            actual_item = q.front()
+            expected_item, _ = expected_item_and_key
+            assert expected_item == actual_item
+            actual_item = q.pop()
+            assert expected_item == actual_item
+            assert q.size == total_queue_size - i - 1
