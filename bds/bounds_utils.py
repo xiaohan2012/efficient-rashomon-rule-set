167bb58ed0d8125f5c4d7a9cf9aed487b02799b8
Han Xiao <xiaohan2012@gmail.com>
2023-04-26 07:41:56 +0800

    black formatting

--- bds/bounds_utils.py
+++ bds/bounds_utils.py
@@ -1,6 +1,7 @@
 import numpy as np
-import pandas as pd 
+import pandas as pd
 from .cache_tree import CacheTree, Node
+
 
 class EquivalentPointClass:
     """a single class of points all having the same attributes"""
@@ -8,60 +9,54 @@ class EquivalentPointClass:
     def __init__(self, this_id, attrs):
         self.id = this_id
         self.attrs = attrs
-        self.total_positives = 0 
-        self.total_negatives = 0 
-        self.minority_mistakes = 0 
+        self.total_positives = 0
+        self.total_negatives = 0
+        self.minority_mistakes = 0
 
-    def update(self, label): 
-        if label==1: 
-            self.total_positives+=1 
-        else: 
-            self.total_negatives+=1 
-        
-        self.minority_mistakes = min( self.total_negatives, self.total_positives ) # recall this is to be normalized 
+    def update(self, label):
+        if label == 1:
+            self.total_positives += 1
+        else:
+            self.total_negatives += 1
+
+        self.minority_mistakes = min(
+            self.total_negatives, self.total_positives
+        )  # recall this is to be normalized
 
 
-def find_equivalence_classes(X_trn : pd.DataFrame, y_train: np.ndarray): 
-    
-    '''
-    Fimd equivalence classes of points having the same attributes but possibly different labels. 
-    This function is to be used once prior to branch-and-bound execution to exploit the equivalence-points-based bound. 
-    
+def find_equivalence_classes(X_trn: pd.DataFrame, y_train: np.ndarray):
+    """
+    Fimd equivalence classes of points having the same attributes but possibly different labels.
+    This function is to be used once prior to branch-and-bound execution to exploit the equivalence-points-based bound.
+
     Parameters
     ----------
     X_trn : pd.DataFrame
-       train data attribute matrix 
-       
-    
+       train data attribute matrix
+
+
     y_train :  np.ndarray
-        labels 
+        labels
 
 
     Returns
     -------
     all equivalnce classes of points all_classes
-    '''
-    
+    """
+
     X_trn = X_trn.to_numpy()
-    
-    # find equivalence classes 
+
+    # find equivalence classes
     all_classes_ids = set()
-    all_classes = dict() 
-    for i, point in enumerate(X_trn): 
+    all_classes = dict()
+    for i, point in enumerate(X_trn):
         attrs = np.where(point == 1)[0]
         attr_str = "-".join(map(str, attrs))
-        if attr_str not in all_classes_ids: # new equivalence class 
-            all_classes_ids.add(attr_str) 
+        if attr_str not in all_classes_ids:  # new equivalence class
+            all_classes_ids.add(attr_str)
             all_classes[attr_str] = EquivalentPointClass(attr_str, attrs)
-    
-        
-        else: # update existing equivalence class 
+
+        else:  # update existing equivalence class
             all_classes[attr_str].update(y_train[i])
-            
-            
-        
+
     return all_classes
-        
-        
-        
-        
