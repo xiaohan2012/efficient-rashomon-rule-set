bfd486af789f17b863e11687b6f785746a4ae254
Han Xiao <xiaohan2012@gmail.com>
2023-05-10 11:35:27 +0800

    first version of extended_rref for gf2

--- bds/gf2.py
+++ bds/gf2.py
@@ -6,95 +6,72 @@ from typing import Tuple, Union, Optional
 GF = galois.GF(2)
 
 
+def extended_rref(A: GF, b: GF, verbose: bool=False) -> Tuple[GF, GF, int]:
+    """
+    given a 2D matrix A and a vector b, both in GF2,
+    obtain the reduced row echelon form of a matrix A and repeat the reduction process on a column vector b
+    return the transformed A and b as well as the rank
+    """
+    if b.ndim == 1:
+        b = b.reshape(-1, 1)  # transform it to column vector
+        
+    if (not b.ndim == 2) or b.shape[0] != A.shape[0] or b.shape[1] != 1:
+        raise ValueError(f'b (of shape {b.shape}) should be a 2D column vector of the same number of rows as A')        
+
+    if not A.ndim == 2:
+        raise ValueError(f"Only 2-D matrices can be converted to reduced row echelon form, not {A.ndim}-D.")
+
+    ncols = A.shape[1]
+    A_rre = A.copy()
+    b_rre = b.copy()
+    p = 0  # The pivot
+
+    if verbose:
+        print(f"A:\n{A_rre}")
+        print(f"b:\n{b_rre}")
+    for j in range(ncols):
+        if verbose:
+            print(f'p={p}')
+        # Find a pivot in column `j` at or below row `p`
+        idxs = np.nonzero(A_rre[p:, j])[0]
+        if idxs.size == 0:
+            continue
+        i = p + idxs[0]  # Row with a pivot
+        if verbose:
+            print(f'checking column {j}')            
+            print(f'pivot is at row {i}')
+
+        # Swap row `p` and `i`. The pivot is now located at row `p`.
+        A_rre[[p, i], :] = A_rre[[i, p], :]
+        b_rre[[p, i], :] = b_rre[[i, p], :]
+        if p != i and verbose:
+            print(f'swap row {p} and {i}')
+            print("A:")
+            print(f'{A_rre}')
+            print("b:")
+            print(f'{b_rre}')
+
+        idxs = np.nonzero(A_rre[:, j])[0].tolist()
+        idxs.remove(p)
+        A_rre[idxs, :] -= A_rre[p, :]
+        b_rre[idxs, :] -= b_rre[p, :]
+        
+        if len(idxs) > 0 and verbose:
+            print('Force zeros above and below the pivot')            
+            print(f'substract row {p} to row {idxs}')
+            print("A:")
+            print(A_rre)
+            print("b:")
+            print(b_rre)
+
+        p += 1
+        if p == A_rre.shape[0]:
+            break
+
+    return A_rre, b_rre.flatten(), p
+
 def eye(rank: int) -> GF:
     return GF(np.eye(rank, dtype=int))
-
-
-def rref_of_triu(
-    U: GF, return_E: bool = True, verbose: int = -1
-) -> Union[GF, Tuple[GF, GF]]:
-    """find the reduced row echelon form of an upper triangular matrix U in GF(2)
-
-    return the elimination matrix if return_E is True
-    """
-    assert np.allclose(np.asarray(U), np.triu(U)), "the matrix is not upper triangular"
-
-    U_acc = U.copy()
-
-    nrow, ncol = U.shape
-    # E_list = [] # list of elementary matrices
-    E_acc = eye(nrow)  # accumulated elimination matrix
-    for i in range(nrow - 1, -1, -1):
-        if verbose > 0:
-            print("at row", i)
-        # find pivot -- the left most non-zero entry
-        pivot_pos = -1
-        for j in range(0, ncol):
-            if U[i, j] != 0:
-                pivot_pos = j
-                break
-
-        if not (0 <= pivot_pos < ncol):
-            if verbose > 0:
-                print("the row is all zero")
-            continue
-
-        if verbose > 0:
-            print("pivot_pos", pivot_pos)
-
-        # look upward
-        for k in range(i - 1, -1, -1):
-            if verbose > 0:
-                print("checking row ", k)
-            if U[k, pivot_pos] != 0:
-                # we need to perform row elimination
-                E = eye(nrow)
-
-                assert i != k
-                # subtract row i from row k
-                if verbose > 0:
-                    print("subtract row {} from {}".format(i, k))
-                E[k, i] = 1
-                E_acc = E @ E_acc
-
-                if verbose > 0:
-                    print("E:\n {}".format(E))
-
-                    U_acc = E @ U_acc
-                    print("U_acc:", U_acc)
-    rref = E_acc @ U
-    if not return_E:
-        return rref
-    else:
-        return rref, E_acc
-
-
-def rref(A: GF, b: Optional[GF] = None) -> Tuple[GF, GF]:
-    """given a linear system Ax=b or a matrix A in GF2
-
-    calculate the reduced row echelon form of A (and the corresponding vector of b if b is given)
-    """
-    # do Gaussian elimination, PA = LU
-    # P: the permutation matrix
-    # L: the inverse of row elimination matrix
-    # U: the upper triangular matrix of A
-    P, L, U = A.plu_decompose()
-
-    # get rref of upper triangular matrix
-    R, E = rref_of_triu(U, return_E=True, verbose=-1)
-
-    if b is not None:
-        # find c
-        # PAx = Pb -> LUx = Pb = Lc
-        c = np.linalg.solve(L, P @ b)  # solve Lc = Pb
-
-        # replay the backward elimination on c
-        d = E @ c
-
-        return R, d
-    else:
-        return R
-
 
 def is_piecewise_linear(arr: GF):
     """
--- tests/test_gf2.py
+++ tests/test_gf2.py
@@ -1,131 +1,144 @@
 import pytest
 import numpy as np
 from bds import gf2
-from bds.gf2 import rref_of_triu, GF, rref
+from bds.gf2 import extended_rref, GF, eye as gf2_eye
 from bds.utils import randints
 
 np.random.seed(12345)
 
 
-class Test_rref_triu:
-    def test_toy(self):
-        U = GF([[1, 0, 0, 0, 1], [0, 1, 1, 1, 1], [0, 0, 1, 0, 0], [0, 0, 0, 1, 1]])
-        actual_rref_U, actual_E = rref_of_triu(U, return_E=True)
+class TestExtendedRref:
+    @pytest.mark.parametrize('b', [
+        GF([[[0, 0], [1, 1]]]), # 3D
+        GF([[[0], [0], [0], [0]]]), # 2D, shape mismatch
+        GF([0, 0, 1, 1]), # 1D, too long
+    ])
+    def test_invalid_inputs(self, b):
+        A = GF([[0, 1, 0], [1, 1, 0], [1, 0, 1]])
+        
+        with pytest.raises(ValueError):
+            extended_rref(A, b)
+        
+        
 
-        # checking rref matrix
-        expected_rref_U = GF(
-            np.array(
-                [[1, 0, 0, 0, 1], [0, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 1, 1]]
-            )
-        )
-
-        np.testing.assert_equal(actual_rref_U, expected_rref_U)
-
-        # checking elimination matrix
-        # subtract row 3 from row 1
-        E1 = GF(np.array([[1, 0, 0, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]]))
-        # substract row 2 from row 1
-        E2 = GF(np.array([[1, 0, 0, 0], [0, 1, 1, 0], [0, 0, 1, 0], [0, 0, 0, 1]]))
-
-        # the overall elimination matrix is the product between the individual elimination matrices
-        expected_E = E2 @ E1
-        np.testing.assert_equal(actual_E, expected_E)
-
-    def test_eye_like(self):
-        rank = 5
-        I = gf2.eye(rank)
-        R, E = rref_of_triu(I)
-        np.testing.assert_equal(R, I)
-        np.testing.assert_equal(E, I)
-
-        # input is a fat eye-like matrix
-        # of shape rank x (2 rank)
-        fat_I = GF(
-            np.hstack((np.eye(rank, dtype=int), np.zeros((rank, rank), dtype=int)))
-        )
-        R, E = rref_of_triu(fat_I)
-        np.testing.assert_equal(R, fat_I)
-        np.testing.assert_equal(E, I)
-
-        # input is a thin eye-like matrix
-        # of shape (2 rank) x rank
-        thin_I = GF(
-            np.vstack((np.eye(rank, dtype=int), np.zeros((rank, rank), dtype=int)))
-        )
-        R, E = rref_of_triu(thin_I)
-        np.testing.assert_equal(R, thin_I)
-        np.testing.assert_equal(E, gf2.eye(rank * 2))
-
-    @pytest.mark.parametrize("rank", [3, 10, 25])
-    def test_all_ones(self, rank):
-        # generate a full-rank uppertriangular matrix
-        U = GF(np.triu(np.ones((rank, rank), dtype=int)))
-        rref, E = rref_of_triu(U, return_E=True, verbose=True)
-        np.testing.assert_equal(gf2.eye(rank), rref)
-
-    @pytest.mark.parametrize("seed", randints(5))
-    def test_if_satsifying_structural_requirements(self, seed):
-        """for a rref, at each pivot, the entries above should be zero"""
-        rank = 10
-
-        # generate a full-rank uppertriangular matrix
-        A = GF.Random((rank, rank), seed=4321)
-        U = np.triu(A)
-        for i in range(rank):
-            U[i, i] = 1  # make it full rank
-        U = GF(U)
-        rref = rref_of_triu(U, return_E=False, verbose=True)
-
-        np.testing.assert_equal(gf2.eye(rank), rref)
-
-
-class Test_rref:
-    def test_toy_1(self):
-        A = GF([[1, 0, 0, 0, 1], [0, 1, 1, 1, 1], [1, 1, 1, 0, 1], [0, 1, 0, 1, 1]])
-
-        b = GF([1, 0, 0, 1])
-
-        R, d = rref(A, b)
-
-        expected_R = GF(
-            [[1, 0, 0, 0, 1], [0, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 1, 1]]
-        )
-        expected_d = GF([1, 0, 1, 1])
-
-        np.testing.assert_equal(R, expected_R)
-        np.testing.assert_equal(d, expected_d)
-
-    def test_toy_2(self):
-        A = GF([[1, 1, 1, 1], [0, 1, 1, 1], [0, 0, 1, 1], [0, 0, 0, 1]])
-
-        expected_R = GF([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-
-        # try different b values
-        b_and_d = [
-            (GF([1, 1, 1, 1]), GF([0, 0, 0, 1])),
-            (GF([1, 1, 1, 0]), GF([0, 0, 1, 0])),
-            (GF([1, 1, 0, 0]), GF([0, 1, 0, 0])),
-        ]
-
-        for b, expected_d in b_and_d:
-            R, d = rref(A, b)
-            np.testing.assert_equal(R, expected_R)
-            np.testing.assert_equal(d, expected_d)
-
-    def test_eye_like(self):
-        rank = 5
-        I = gf2.eye(rank)
-        R = rref(I)
-        np.testing.assert_equal(R, I)
-
-        # input is a fat eye-like matrix
-        # of shape rank x (2 rank)
-        fat_I = GF(
-            np.hstack((np.eye(rank, dtype=int), np.zeros((rank, rank), dtype=int)))
-        )
-        R = rref(fat_I)
-        np.testing.assert_equal(R, fat_I)
-
-        # ignore the thin case since L is not defined
-        # input is a thin eye-like matrix
-        # of shape (2 rank) x rank
+    @pytest.mark.parametrize(
+        "A, b, exp_A, exp_b, exp_p",
+        [
+            # expected reduction process and per-step output
+            # 1. at column 0
+            #    swap row 0 and row 1
+            #    A:
+            #     [1, 1, 0],
+            #     [0, 1, 0],
+            #     [1, 0, 1]
+            #    b:
+            #     [0]
+            #     [1]
+            #     [0]
+            #    -- no need to force 1 on the pivot
+            # 2. at column 1
+            #   pivot is at row 1
+            #   no need to swap
+            #   force above and below to be zero
+            #     A:
+            #     [1, 0, 0],
+            #     [0, 1, 0],
+            #     [0, 0, 1]
+            #     b:
+            #     [1]
+            #     [1]
+            #     [1]
+            # 3. at column 2
+            #   pivot is at row 2
+            #   no need to swap
+            #   no need to force above and below to be zero
+            # done with p = 3
+            (
+                GF([[0, 1, 0], [1, 1, 0], [1, 0, 1]]),  # A
+                GF([1, 0, 0]),  # b
+                gf2_eye(3),  # expected A
+                GF([1, 1, 1]),  # expected b
+                3,  # rank
+            ),
+            # ---------------------------
+            # 1. at column 0
+            #    swap row 0 and row 1
+            #    A:
+            #    [1, 1]
+            #    [0, 0]
+            #    b:
+            #     [0]
+            #     [1]
+            # 2. at column 1
+            #    there is no pivot
+            # done
+            # we return with p = 1 and A is eye matrix
+            (GF([[0, 0], [1, 1]]), GF([1, 0]), GF([[1, 1], [0, 0]]), GF([0, 1]), 1),
+            # ---------------------------
+            # 1. at column 0
+            #    swap row 0 and row 1
+            #    A:
+            #    [1, 0, 1]
+            #    [0, 0, 1]
+            #    b:
+            #     [0]
+            #     [1]
+            # 2. at column 1
+            #    there is no pivot
+            # 3. at column 2
+            #    substract row 0 by row 1
+            #    A:
+            #    [1, 0, 0]
+            #    [0, 0, 1]
+            #    b:
+            #     [1]
+            #     [1]
+            # done with p = 2
+            (
+                GF(
+                    [
+                        [0, 0, 1],
+                        [1, 0, 1],
+                    ]
+                ),
+                GF([1, 0]),
+                GF([[1, 0, 0], [0, 0, 1]]),
+                GF([1, 1]),
+                2,
+            ),
+            # ---------------------------
+            # 1. at column 0
+            #    swap row 0 and row 1
+            #    A:
+            #     [1, 0]
+            #     [0, 0]
+            #     [0, 1]
+            #    b:
+            #    [0]
+            #    [1]
+            #    [0]
+            # 2. at column 1
+            #   swap row 1 and row 2
+            #   A:
+            #     [1, 0]
+            #     [0, 1]
+            #     [0, 0]
+            #   b:
+            #    [0]
+            #    [0]
+            #    [1]
+            # done with p = 2
+            (
+                GF([[0, 0], [1, 0], [0, 1]]),
+                GF([1, 0, 0]),
+                GF([[1, 0], [0, 1], [0, 0]]),
+                GF([0, 0, 1]),
+                2,
+            ),
+        ],
+    )
+    def test_valid_inputs(self, A, b, exp_A, exp_b, exp_p):
+        A_rref, b_rref, p = extended_rref(A, b, verbose=False)
+        assert (A_rref == exp_A).all()
+        assert (b_rref == exp_b).all()
+        assert p == exp_p
