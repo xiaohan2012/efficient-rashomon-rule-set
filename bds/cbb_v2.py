num_rules calculation fix and minor update to assign_pivot_variables

bds/cbb_v2.py
@@ -91,6 +91,8 @@ def assign_pivot_variables(
     z: np.ndarray,
     t: np.ndarray,
     A: np.ndarray,
+    # neg_A_indices: np.ndarray,
+    # neg_A_indptr: np.ndarray,
     max_nz_idx_array: np.ndarray,
     row2pivot_column: np.ndarray,
 ) -> np.ndarray:
@@ -100,30 +102,22 @@ def assign_pivot_variables(
     # if (j - 1) in set(row2pivot_column):
     #     raise ValueError(f"cannot set pivot variable of column {j - 1}")
 
-    # if j > 0:
-    #     cst_idxs = A_indices[
-    #         A_indptr[j - 1] : A_indptr[j]
-    #     ]  # get the constraint (row) indices corresponind to rule j
-    # else:
-    #     cst_idxs = np.array([], dtype=int)
+    # irrelevant_cst_idxs = neg_A_indices[
+    #     neg_A_indptr[j - 1] : neg_A_indptr[j]
+    # ]  # get the constraint (row) indices where rule j is absent/irrelevant
 
-    # print("cst_idxs: {}".format(cst_idxs))
-    selected_rules = np.empty(A.shape[0], np.int_)
+    selected_rules = np.empty(A.shape[1], np.int_)
     num_rules_selected = 0
 
     for i in range(rank):  # loop up to rank
         max_nz_idx = max_nz_idx_array[i]
-        # print("i: {}".format(i))
-        # print("max_nz_idx: {}".format(max_nz_idx))
-        # print("z[i]: {}".format(z[i]))
-        # print("t[i]: {}".format(t[i]))
 
-        if (
-            # the ith constraint is not finalized by j
-            (j < (max_nz_idx + 1))
-            # j is not the default rule and jth rule is not relevant for the ith constraint
-            or (j > 0 and A[i][j - 1] == 0)
-        ) and (z[i] != t[i]):
+        if (z[i] != t[i]) and (
+            # the rule is not relevant
+            (A[i][j - 1] == 0)
+            # the ith constraint is not interior
+            or (j < (max_nz_idx + 1))
+        ):
             # +1 because rule is 1-indexed
             selected_rules[num_rules_selected] = row2pivot_column[i] + 1
             num_rules_selected += 1
@@ -233,18 +227,11 @@ class ConstrainedBranchAndBound(ConstrainedBranchAndBoundNaive):
         self.free_rule_idxs = (
             set(map(lambda v: v + 1, range(self.num_vars))) - self.pivot_rule_idxs
         )
-        # # a rule j is unconstrained if it is not involved in any constraint, i.e., A[i, j-1] == 0 for all i
-        # # print("self.A.astype(int):\n{}".format(self.A.astype(int)))
-        # self.unconstrained_rule_idxs = set((self.A.sum(axis=0) == 0).nonzero()[0] + 1)
         # mapping from row index to the pivot column index
         self.row2pivot_column = np.array(self.pivot_columns, dtype=int)
 
         # a rule is a border rule if it appears as the last rule in at least one constraint
         self.border_rule_idxs = set(self.max_nz_idx_array + 1) - {0}
-        # print("self.row2pivot_column: {}".format(self.row2pivot_column))
-        # print("self.A:\n {}".format(self.A.astype(int)))
-        # print("self.t:\n {}".format(self.t.astype(int)))
-        # logger.debug(f"num. unconstrained rules: {len(self.unconstrained_rule_idxs)}")
 
     def reset_queue(self):
         self.queue: Queue = Queue()
@@ -393,16 +380,6 @@ class ConstrainedBranchAndBound(ConstrainedBranchAndBoundNaive):
 
                 # logger.debug("[assign_pivot_variables] e2 = {}".format(e2_idxs))
                 # captured by the prefix, current rule, and rules introduced by update_pivot_variables
-
-                # a verbose way to do bitwise inverse on u
-                # the ~ operator returns a negative number, e.g., -0b1000000000
-                # and applying lor gives weird result
-                # TODO: is there a way to do bitwise inverse in the "expected" way
-                # def mpz_comp(v):
-                #     not_v = v
-                #     for i in range(len(self.rules)):
-                #         not_v = gmp.bit_flip(not_v, i)
-                #     return not_v
 
                 not_u = ~u
                 w = v1 | not_u

bds/gf2.py
@@ -6,7 +6,9 @@ from typing import Tuple, Union, Optional, List
 GF = galois.GF(2)
 
 
-def extended_rref(A: GF, b: GF, verbose: bool = False) -> Tuple[GF, GF, int, np.ndarray]:
+def extended_rref(
+    A: GF, b: GF, verbose: bool = False
+) -> Tuple[GF, GF, int, np.ndarray]:
     """
     given a 2D matrix A and a vector b, both in GF2,
     obtain the reduced row echelon form of a matrix A and repeat the reduction process on a column vector b
@@ -119,7 +121,7 @@ def is_piecewise_linear(arr: GF):
 
 def num_of_solutions(A: GF, b: GF) -> int:
     """return the number of solutions to a linear system Ax=b in GF2"""
-    R, t, rank = extended_rref(A, b)
+    R, t, rank, _ = extended_rref(A, b)
     if not (t[rank:] == 0).all():
         # the system is not solvable
         return 0
@@ -148,3 +150,10 @@ def fix_variables_to_one(A: GF, b: GF, which: List[int]) -> Tuple[GF, GF]:
     Ap = np.delete(A, which, axis=1)
 
     return Ap, bp
+
+
+def negate_all(A: Union[GF, np.ndarray]) -> GF:
+    """negate the all entries in the array-like object A"""
+    if isinstance(A, np.ndarray):
+        A = GF(A)
+    return GF(np.ones(A.shape, dtype=int)) + A

tests/test_gf2.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 
 from bds import gf2
-from bds.gf2 import GF, extended_rref, fix_variables_to_one, num_of_solutions
+from bds.gf2 import GF, extended_rref, fix_variables_to_one, num_of_solutions, negate_all
 from bds.gf2 import eye as gf2_eye
 from bds.utils import randints
 
@@ -180,3 +180,22 @@ def test_fix_variables_to_one(which, expected_t):
 )
 def test_num_of_solutions(A, b, expected):
     assert num_of_solutions(A, b) == expected
+
+@pytest.mark.parametrize(
+    'A, expected',
+    [
+        (np.array([0, 0, 1], dtype=int), GF([1, 1, 0])),
+        (np.array([1, 1, 1], dtype=int), GF([0, 0, 0])),
+        (np.array([0, 0, 0], dtype=int), GF([1, 1, 1])),
+        (GF([0, 0, 1]), GF([1, 1, 0])),
+        (GF([1, 1, 1]), GF([0, 0, 0])),
+        (GF([0, 0, 0]), GF([1, 1, 1])),
+        (np.array([[1, 1, 1], [1, 1, 1]], dtype=int), GF([[0, 0, 0], [0, 0, 0]]))
+    ]
+)  
+def test_negate_all(A, expected):
+    assert isinstance(expected, GF)
+    np.testing.assert_allclose(
+        np.array(negate_all(A), dtype=int),
+        np.array(expected, dtype=int)
+    )

tests/utils.py
@@ -19,7 +19,11 @@ def assert_dict_allclose(actual: Dict[Any, Number], expected: [Any, Number]):
     assert actual_keys == expected_keys, f"{actual_keys} != {expected_keys}"
 
     for k in actual.keys():
-        np.testing.assert_allclose(float(actual[k]), float(expected[k]), err_msg=k)
+        np.testing.assert_allclose(
+            np.array(actual[k], dtype=float),
+            np.array(expected[k], dtype=float),
+            err_msg=k
+        )
 
 
 def generate_random_rule_list(num_pts: int, num_rules: int, rand_seed: int = None):