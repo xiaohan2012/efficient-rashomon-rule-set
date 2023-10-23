6529473d61f8b853d91a7a7521d3800000d5c01a
Han Xiao <xiaohan2012@gmail.com>
2023-10-23 12:29:03 +0300

    a function to extract candidate rules based on min support

---
+++ bds/candidate_generation.py
@@ -0,0 +1,33 @@
+import numpy as np
+from typing import List
+from bds.fpgrowth import preprocess_transaction_list, build_fptree, fpgrowth_on_tree
+from bds.utils import compute_truthtable
+from bds.rule import Rule
+
+
+def extract_rules_with_min_support(
+    X: np.ndarray, attribute_names: List[str], min_support: int
+) -> List[Rule]:
+    """given a binarized feature matrix X, extract all association rules whoe support is at least min_support
+
+    the list of returned rules are sorted by their support in ascending order
+    """
+    X_bag = [set([j for j, x in enumerate(row) if x]) for row in X]
+
+    ordered_input_data = preprocess_transaction_list(X_bag, min_support)
+    tree = build_fptree(ordered_input_data)
+    frequent_itemsets = set(fpgrowth_on_tree(tree, set(), min_support))
+
+    # Now create rules
+    all_rules = []
+    for i, itemset in enumerate(frequent_itemsets):
+        rule = Rule(
+            id=i,
+            name="rule_" + str(i),
+            cardinality=len(itemset),
+            truthtable=compute_truthtable(X, itemset),
+            predicates=[attribute_names[idx] for idx in itemset],
+        )
+        all_rules.append(rule)
+
+    return list(sorted(all_rules, key=lambda r: r.support, reverse=True))
