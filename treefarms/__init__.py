a30bbbe86699d5c0073989f135973d1c440d78ac
Han Xiao <xiaohan2012@gmail.com>
2024-06-03 22:10:59 +0300

    rm treefarms

--- treefarms/__init__.py
+++
@@ -1,4 +0,0 @@
-# We're just going to bring these to the front
-# This is Tynan guessing what
-from treefarms.model.treefarms import TREEFARMS
-from treefarms.model.threshold_guess import get_thresholds
